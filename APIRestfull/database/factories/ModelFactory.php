2483b212c50a6f9535c37ae8c30e9c5dd098679a
Alejandro <alexrodriguezserrano1595@gmail.com>
2018-04-15 12:47:08 -0500

    Implements seed

--- APIRestfull/database/factories/ModelFactory.php
+++ APIRestfull/database/factories/ModelFactory.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Centro_medico;
+use App\Enfermeras;
 use App\Medicos;
+use App\Pacientes;
 use App\Suscripciones;
 use App\Usuarios_sistema;
 
@@ -22,56 +24,94 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'lastName' => $faker->lastName,
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
     ];
-});*/
+});
 
 $factory->define(Centro_medico::class, function (Faker\Generator $faker) {
 
     return [
-        'Nombre'    => $faker->word,
-        'Direccion' => $faker->address,
-        'Estado'    => randomElement([Centro_medico::ACTIVA, Centro_medico::NO_ACTIVO]),
+        'Nombre'    => $faker->lastName,
+        'Direccion' => $faker->state,
+        'Estado'    => $faker->randomElement([Centro_medico::ACTIVO, Centro_medico::NO_ACTIVO]),
     ];
-});
-
+});*/
 
 $factory->define(Medicos::class, function (Faker\Generator $faker) {
 
 	$Centro_medico = Centro_medico::all()->random();
 
     return [
-        'Nombre'           => $faker->firstNameMale,
+        'Nombre'           => $faker->lastName,
         'Apellido' 		   => $faker->lastName,
     	'Especialidad'     => $faker->company,
         'Sexo'    	       => $faker->randomElement([Medicos::MALE, Medicos::FEMALE]),
-        'Edad' 		       => $faker->random_int(20, 40),
-        'Cedula' 	       => $faker->str_random(10),
-        'Direccion' 	   => $faker->address,
+        'Edad' 		       => $faker->numberBetween($min = 20, $max = 40),
+        'Cedula' 	       => str_random(10),
+        'Direccion' 	   => $faker->state,
         'id_Centro_medico' => $faker->$Centro_medico->id,
         'Estado' 		   => $faker->randomElement([Medicos::ACTIVO, Medicos::NO_ACTIVO]),
 
     ];
 });
 
+$factory->define(Enfermeras::class, function (Faker\Generator $faker) {
+
+	$Centro_medico = Centro_medico::all()->random();
+
+    return [
+        'Nombre'           => $faker->lastName,
+        'Apellido' 		   => $faker->lastName,
+    	'Especialidad'     => $faker->company,
+        'Sexo'    	       => $faker->randomElement([Medicos::MALE, Medicos::FEMALE]),
+        'Edad' 		       => $faker->numberBetween($min = 20, $max = 40),
+        'Cedula' 	       => $faker->str_random(10),
+        'Direccion' 	   => $faker->state,
+        'id_Centro_medico' => $faker->$Centro_medico->id,
+        'Estado' 		   => $faker->randomElement([Enfermeras::ACTIVO, Enfermeras::NO_ACTIVO]),
+
+    ];
+});
+
+
+$factory->define(Pacientes::class, function (Faker\Generator $faker) {
+
+	$Centro_medico = Centro_medico::all()->random();
+
+    return [
+        'Nombre'            => $faker->lastName,
+        'Apellido' 		    => $faker->lastName,
+        'Telefono' 		    => $faker->phoneNumber,
+        'Sexo'    	        => $faker->randomElement([Medicos::MALE, Medicos::FEMALE]),
+        'Edad' 		        => $faker->numberBetween($min = 20, $max = 40),
+        'Direccion' 	    => $faker->state,
+        'Tipo_sangre' 	    => $faker->str_random(4),
+        'id_Centro_medico'  => $faker->$Centro_medico->id,
+        'Fecha_inscripcion' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'Estado' 		    => $faker->randomElement([Pacientes::ACTIVO, Pacientes::NO_ACTIVO]),
+
+    ];
+});
+
+
 
 $factory->define(Usuarios_sistema::class, function (Faker\Generator $faker) {
 	static $password;
 
     return [
-        'Usuario' 		      => $faker->name,
+        'Usuario' 		      => $faker->lastName,
         'Password' 			  => $password ?: $password = bcrypt('secret'),
-        'Email' 			  => $faker->unique()->safeEmail, 
+        'Email' 			  => $faker->safeEmail->unique(), 
         'Fecha_registro' 	  => $faker->date,
         'Token_verificacion'  => Usuarios_sistema::generateToken(),
         'Verificada'		  => $faker->randomElement(Usuarios_sistema::VERIFICADA, Usuarios_sistema::NO_VERIFICADA),
-        'idMedico' 		      => $faker->word,
-        'idEnfermera'         => $faker->word,
-        'idCentro_medico'     => $faker->word,
-        'idTipo_usuario'      => $faker->word,
+        'idMedico' 		      => $faker->numberBetween($min = 0, $max = 50),
+        'idEnfermera'         => $faker->numberBetween($min = 0, $max = 50),
+        'idCentro_medico'     => $faker->numberBetween($min = 0, $max = 50),
+        'idTipo_usuario'      => $faker->randomElement([Usuarios_sistema::SUPERADMIN, Usuarios_sistema::ADMINISTRADOR, Usuarios_sistema::MEDICO, Usuarios_sistema::ENFERMERA, Usuarios_sistema::PACIENTE, Usuarios_sistema::CAJERO]),
         'Estado'              => $faker->randomElement([Usuarios_sistema::ACTIVA, Usuarios_sistema::NO_ACTIVA]),
 
     ];
@@ -79,16 +119,17 @@ $factory->define(Usuarios_sistema::class, function (Faker\Generator $faker) {
 
 
 $factory->define(Suscripciones::class, function (Faker\Generator $faker) {
+	$Centro_medico = Centro_medico::all()->random();
 
     return [
-        'Tipo_suscripcion'    => $faker->word,
-        'Nombre_persona' => $faker->paragraph(1),
-        'Apellidos_persona'    => $faker->randomElement([Centro_medico::ACTIVA, Centro_medico::NO_ACTIVO]),
-        'Fecha_inscripcion' => $faker,
-        'Cedula' => $faker,
-        'idCentro_medico' => $faker,
-        'idUsuarios_sistema' => $faker,
-        'Estado' => $faker,
+        'Tipo_suscripcion'   => $faker->random_int(1,3),
+        'Nombre_persona'     => $faker->lastName,
+        'Apellidos_persona'  => $faker->lastName,
+        'Fecha_inscripcion'  => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'Cedula'             => $faker->str_random(10),
+        'idCentro_medico'    => $Centro_medico->id,
+        'idUsuarios_sistema' => $faker->numberBetween($min = 0, $max = 50),
+        'Estado'             => $faker->randomElement([Suscripciones::ACTIVA, Suscripciones::SUSPENDIDA, Suscripciones::ELIMINADA]),
     ];
 });
 
--- APIRestfull/database/seeds/DatabaseSeeder.php
+++ APIRestfull/database/seeds/DatabaseSeeder.php
@@ -5,6 +5,8 @@ use App\Centro_medico;
 use App\Enfermeras;
 use App\Infrestructura_Centro_medico;
 use App\Medicos;
+use App\Pacientes;
+use App\Suscripciones;
 use App\Tipo_usuario;
 use App\Usuarios_sistema;
 use Illuminate\Database\Seeder;
@@ -20,18 +22,35 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         
-    	//Desactivamos la revicion de las llaves foraneas.
-        DB::statement('SET FOREIGN_KEYS_CHECKS = 0');
+    	//Desactivamos la revision de las llaves foraneas.
+        DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         
         //trunco las tablas para poder meter datos nuevos
+
+        //Centro_medico::truncate();
         Camas_x_piso::truncate();
-        Centro_medico::truncate();
         Enfermeras::truncate();
         Infrestructura_Centro_medico::truncate();
         Medicos::truncate();
+        Pacientes::truncate();
         Suscripciones::truncate();
         Tipo_usuario::truncate();
         Usuarios_sistema::truncate();
 
+        //Variables para la cantidad de datos creados por tabla
+
+        $cantidadCentrosMedicos  = 3;
+        $cantidadMedicos         = 50;
+        $cantidadEnfermeras      = 50;
+        $cantidadPacientes       = 50;
+        $cantidadUsuariosSistema = 50;
+        $cantidadSuscripciones   = 3;
+
+        //factory(Centro_medico::class, $cantidadCentrosMedicos)->create();
+        factory(Medicos::class, $cantidadMedicos)->create();
+        factory(Enfermeras::class, $cantidadEnfermeras)->create();
+        factory(Pacientes::class, $cantidadPacientes)->create();
+        factory(Usuarios_sistema::class, $cantidadUsuariosSistema)->create();
+        factory(Suscripciones::class, $cantidadSuscripciones)->create();
     }
 }
