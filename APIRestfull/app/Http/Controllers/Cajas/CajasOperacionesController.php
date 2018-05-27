646ef1038512567f5a53fa96d6ef011b0a1d8cfc
Raitoforce <mahgt_34@hotmail.com>
2018-05-27 02:57:11 -0500

    CambiosH

--- APIRestfull/app/Http/Controllers/Cajas/CajasOperacionesController.php
+++ APIRestfull/app/Http/Controllers/Cajas/CajasOperacionesController.php
@@ -21,18 +21,66 @@ class CajasOperacionesController extends ApiController
    		
     	$idpaciente= $_GET['idpaciente'];
    		$paciente= Pacientes::findOrFail($idpaciente);
+   		$consultas = Consultas::where([["idCentro_medico","=",$centroMedico->id],["Fecha","=","$date"],["idPaciente","=",$paciente->id]])->get();
+   		$total = 0;	
+   		foreach ($consultas as $consulta) {
+   			if(empty($consulta->id))
+   				break;
+   			$medico = Medicos::findOrFail($consulta->idMedico);
+   			$newConsulta[]=array('id' => $consulta->id, 
+   								'Fecha' => $consulta->Fecha,
+   								'Costo' => $consulta->Costo,
+   								'Medico' => array(
+   									'id' => $medico->id,
+   									'Nombre' => $medico->Nombre,
+   									'Apellidos' => $medico->Apellidos
+   								),
+   							);
+   			$total += $consulta->Costo;
 
-   		$idmedico= $_GET['idmedico'];
-   		$medico= Medicos::findOrFail($idmedico);
+   			$vacunas = Vacunas_x_paciente::where("idConsulta","=",$consulta->id)->get();
 
-   		$cirugias = Cirugias_x_paciente::where([["idMedico","=",$medico->id],["idCentro_medico","=",$centroMedico->id],["Fecha_egreso","=",$date]])->get();
-   		$consultas = Consultas::where([["idMedico","=",$medico->id],["idCentro_medico","=",$centroMedico->id],["Fecha","=",$date]])->get();
-   		print_r($consultas);
-   		$vacunas = Vacunas_x_paciente::where([["idConsulta","=",$consultas->id],["idCentro_medico","=",$centroMedico->id],["Fecha_aplicacion","=",$date]])->get();
+   			foreach ($vacunas as $vacuna) {
+   				if(empty($vacuna))
+   					break;
+
+   				$v = Vacunas::findOrFail($vacuna->idVacuna);
+   				$newVacunas[]=array('Vacuna' => array('Nombre' => $v->Nombre,
+   													'Costo' => $v->Costo, 
+   					
+   					), 
+   				);
+
+   				$v=Vacuna::findOrFail($vacuna->idVacuna);
 
 
-   		print_r($cirugias);
-   		print_r($consultas);
-   		print_r($vacunas);
+   				$total+=$v->Costo;
+   			}
+   		}
+
+   		$cirugias = Cirugias_x_paciente::where([["idCentro_medico","=",$centroMedico->id],["Fecha_egreso","=",$date],["idPaciente","=",$paciente->id]])->get();
+   		foreach ($cirugias as $cirugia){
+  			if(empty($cirugia->id))break;
+  			$c=Cirugias::findOrFail($cirugia->idCirugia);
+  			$medico= Medico::findOrFail($cirugia->idMedico);
+  			$newCirugias[] = array('Cirugia'=>array('Nombre' => $c->Nombre,
+  													'Costos' => $c->Costos,
+  				),
+  				'Medico' => array(
+   									'id' => $medico->id,
+   									'Nombre' => $medico->Nombre,
+   									'Apellidos' => $medico->Apellidos
+   								), 
+  			);
+   			$total+=$c->Costos;
+   		}
+   		return $this->showList(
+   			array(
+   				'nombrePaciente' => $paciente->Nombre." ".$paciente->Apellidos,
+   				'pago' 		     => $total,
+   				'cirugias' 		 => empty($newCirugias) ? ' ' : $newCirugias,
+   				'consultas'		=> empty($newConsulta) ? ' ' : $newConsulta,
+   				'vacunas'		=> empty($newVacunas) ? ' ' : $newVacunas
+   			));
     }
 }
